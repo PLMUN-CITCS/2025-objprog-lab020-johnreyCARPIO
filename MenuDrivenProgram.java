fec35e40eec813d74a3b45675665596fe33a4124
John Rey Carpio <carpiojohnrey_bscs@plmun.edu.ph>
2025-02-28 15:08:25 +0800

    Submitting OBJPROG Week 05 - Laboratory # 20

---
+++ MenuDrivenProgram.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class MenuDrivenProgram {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        
+        do {
+            displayMenu();
+            System.out.print("Enter your choice (1-3): ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Please enter a number between 1 and 3.");
+                scanner.next(); // clear invalid input
+                System.out.print("Enter your choice (1-3): ");
+            }
+            choice = scanner.nextInt();
+            handleMenuChoice(choice);
+        } while (choice != 3);
+        
+        scanner.close();
+    }
+    
+    public static void displayMenu() {
+        System.out.println("\nMenu:");
+        System.out.println("1. Greet User");
+        System.out.println("2. Check Even/Odd");
+        System.out.println("3. Exit");
+    }
+    
+    public static void handleMenuChoice(int choice) {
+        switch (choice) {
+            case 1:
+                greetUser();
+                break;
+            case 2:
+                checkEvenOrOdd();
+                break;
+            case 3:
+                System.out.println("Exiting program. Goodbye!");
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a number between 1 and 3.");
+        }
+    }
+    
+    public static void greetUser() {
+        System.out.println("Hello! Welcome!");
+    }
+    
+    public static void checkEvenOrOdd() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input. Please enter an integer.");
+            scanner.next();
+            System.out.print("Enter an integer: ");
+        }
+        int number = scanner.nextInt();
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even number.");
+        } else {
+            System.out.println(number + " is an Odd number.");
+        }
+    }
+}
